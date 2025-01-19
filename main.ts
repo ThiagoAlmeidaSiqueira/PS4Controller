ed2bff6da1d7c5e60ea8356af6652a66176c685d
ThiagoAlmeidaSiqueira <thiagoalmeidasiqueira@gmail.com>
2025-01-18 21:20:05 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
-export enum button {
-    BOTAO_X,
-    BOTAO_CIRCULO,
-    BOTAO_TRIANGULO,
-    BOTAO_QUADRADO,
-    L1,
-    R1
-}
-
-export enum axis {
-    JOY_ESQ_X,
-    JOY_ESQ_Y,
-    JOY_DIR_X,
-    JOY_DIR_Y,
-    L2,
-    R2
-}
-
 //% weight=100 color=#00A0E9 icon="\uf11b" block="PS4 Controller"
 namespace PS4Controller {
+    export enum button_enum {
+        BOTAO_X,
+        BOTAO_CIRCULO,
+        BOTAO_TRIANGULO,
+        BOTAO_QUADRADO,
+        L1,
+        R1
+    }
+    
+    export enum axis_enum   {
+        JOY_ESQ_X,
+        JOY_ESQ_Y,
+        JOY_DIR_X,
+        JOY_DIR_Y,
+        L2,
+        R2
+    }
     const serialDelimiter = "\n";
-    let buttonHandlers: { [key: button]: () => void } = {};
-    let analogHandlers: { [key: axis]: (value: number) => void } = {};
+
+    let buttonHandlers: { [key: string]: () => void } = {};
+    let analogHandlers: { [key: string]: (value: number) => void } = {};
     let connectionHandlers: { [key: string]: () => void } = {};
 
     // Processa os dados recebidos
@@ -31,21 +31,21 @@ namespace PS4Controller {
             let value = parseInt(parts[1]);
 
             // Evento de botão pressionado
-            if (value === 1 && buttonHandlers[key as button]) {
-                buttonHandlers[key as button]();
+            if (value === 1 && buttonHandlers[key]) {
+                buttonHandlerskey;
             }
 
             // Evento de leitura analógica
-            if (!isNaN(value) && analogHandlers[key as axis]) {
-                analogHandlers[key as axis](value);
+            if (!isNaN(value) && analogHandlers[key]) {
+                analogHandlerskey;
             }
 
             // Eventos de conexão/desconexão
             if (key === "CONECTADO" && connectionHandlers["connected"]) {
-                connectionHandlers["connected"]();
+                connectionHandlers"connected";
             }
             if (key === "DESCONECTADO" && connectionHandlers["disconnected"]) {
-                connectionHandlers["disconnected"]();
+                connectionHandlers"disconnected";
             }
         }
     }
@@ -61,8 +61,8 @@ namespace PS4Controller {
      * @param handler A função a ser executada.
      */
     //% block="quando botão $button for pressionado"
-    //% button.defl="BOTAO_X"
-    export function onButtonPressed(button: button, handler: () => void): void {
+    //% button.defl=button_enum.BOTAO_X
+    export function onButtonPressed(button: button_enum, handler: () => void): void {
         buttonHandlers[button] = handler;
     }
 
@@ -71,8 +71,8 @@ namespace PS4Controller {
      * @param handler A função que recebe o valor.
      */
     //% block="quando valor analógico $axis mudar"
-    //% axis.defl="JOY_ESQ_X"
-    export function onAnalogValueReceived(axis: axis, handler: (value: number) => void): void {
+    //% axis.defl=axis_enum.JOY_ESQ_X
+    export function onAnalogValueReceived(axis: axis_enum, handler: (value: number) => void): void {
         analogHandlers[axis] = handler;
     }
 
